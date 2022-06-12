b28198ac2af1937b45b71214ad17a0cc43f51597
Prajwal Deval <prajwalxdeval@gmail.com>
2022-06-12 19:19:18 +0200

    Fixing coordinate system to y vertical & x horizontal. Such that for the images dimension 1 is y and dimension 2 is x (consistent with DaVis and experiment)

--- main.m
+++ main.m
@@ -1,47 +1,59 @@
 clc;
 clear all;
 
+
+%set vars
+windowsize = 32;
+search_size = 0.75;%as proportion of window size
+
 % Reading TIF images
 im_cal = imread('Data/B00001_cal.tif');
 
 im = imread('Data/B00010.tif');
 [size_y,size_x] = size(im);
-height = size_y/2;
-im1 = im(1:height,:);
-im2 = im(height + 1:end,:);
+size_y= size_y/2; %pictures are stacked together so height is actually half
+im1 = im(1:size_y,:);
+im2 = im(size_y + 1:end,:);
 clear im;
 
-% Background computation
+% Background computation, different methodologies - none of these really
+% make sense
 im_min = ((im1+im2) - abs(im1-im2))/2;
 im_ave = (im1 + im2)/2;
 im_sub = im1 - im2;
 
-[xmax, ymax] = size(im1);
 
 % Window size 
-wsize = [32, 32];
-w_width = wsize(1);
-w_height = wsize(2);
+wsize = [windowsize, windowsize];
+w_height = wsize(1);
+w_width = wsize(2);
+
+% Search Window Size
+x_search = w_width*search_size;
+y_search = w_height*search_size;
 
 % Center points grid 
 xmin = w_width/2;
 ymin = w_height/2;
-xgrid = 200:w_width/2:864;
-ygrid = 200:w_height/2:1696;
+xgrid = x_search:w_width/2:size_x-x_search; %starts & ends x&y grid such that
+ygrid = y_search:w_height/2:size_y-y_search;%there is always a search
+                                            %window sized margin outside
 
+
+                                            
+% % % % % CHANGES FIXED UPTO HERE
+                                            
 % Number of windows in total
 w_xcount = length(xgrid);
 w_ycount = length(ygrid);
 
-% Range for search windows
-x_disp_max = w_width/2;
-y_disp_max = w_height/2;
+
 
 xpeakl = 0;
 ypeakl = 0;
 
 test_im1(w_width, w_height) = 0; 
-test_im2(w_width+2*x_disp_max, w_height + 2 * y_disp_max) = 0; 
+test_im2(w_width+2*x_search, w_height + 2 * y_search) = 0; 
 dpx(w_xcount, w_ycount) = 0; 
 dpy(w_xcount, w_ycount) = 0; 
 
@@ -66,8 +78,8 @@ for i=1:(w_xcount)
         [xpeak, ypeak] = find (correlation == max(correlation(:)));
         
         % Re-scaling
-        xpeakl = test_xmin + xpeak - wsize(1)/2 - x_disp_max;
-        ypeakl = test_ymin + ypeak - wsize(2)/2 - y_disp_max;
+        xpeakl = test_xmin + xpeak - wsize(1)/2 - x_search;
+        ypeakl = test_ymin + ypeak - wsize(2)/2 - y_search;
         dpx(i,j) = xpeakl - xgrid(i);
         dpy(i,j) = ypeakl - ygrid(j);
         
